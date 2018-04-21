d2e15ad933e508d1f06b844b571693b13a5b6067
angie_sr06@hotmail.com <angie_sr06@hotmail.com>
2018-04-21 16:29:30 -0500

    webpack

---
+++ bookstore/src/config/firebase.js
@@ -0,0 +1,8 @@
+export default{
+    apiKey: "AIzaSyA6a0vEmD4ZyTr0twQ8pWJVJhjNLgmrZv4",
+    authDomain: "bookstore-f14c1.firebaseapp.com",
+    databaseURL: "https://bookstore-f14c1.firebaseio.com",
+    projectId: "bookstore-f14c1",
+    storageBucket: "bookstore-f14c1.appspot.com",
+    messagingSenderId: "1084861727758"
+};
